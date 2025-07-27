28f7ce33e8784646c6622c815aed5b507de3d95d
thisalsalpura <salpurathisal@gmail.com>
2025-07-27 21:38:37 +0530

    Update 3.1

---
+++ src/java/controller/LoadPendingGigs.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import entity.Gig;
+import entity.Gig_Status;
+import entity.User;
+import entity.User_Status;
+import hibernate.HibernateUtil;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author User
+ */
+@WebServlet(name = "LoadPendingGigs", urlPatterns = {"/LoadPendingGigs"})
+public class LoadPendingGigs extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Gson gson = new Gson();
+        JsonObject responseObject = new JsonObject();
+
+        responseObject.addProperty("status", false);
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        Criteria criteria = session.createCriteria(User_Status.class);
+        criteria.add(Restrictions.eq("value", "Active"));
+
+        if (!criteria.list().isEmpty()) {
+            User_Status user_Status = (User_Status) criteria.list().get(0);
+
+            Criteria criteria1 = session.createCriteria(User.class);
+            criteria1.add(Restrictions.eq("user_Status", user_Status));
+
+            if (!criteria1.list().isEmpty()) {
+                List<User> usersList = criteria1.list();
+
+                Criteria criteria2 = session.createCriteria(Gig_Status.class);
+                criteria2.add(Restrictions.eq("value", "Pending"));
+
+                if (!criteria2.list().isEmpty()) {
+                    Gig_Status gig_Status = (Gig_Status) criteria2.list().get(0);
+
+                    List<Gig> allPendingGigsList = new ArrayList<>();
+                    for (User user : usersList) {
+                        Criteria criteria3 = session.createCriteria(Gig.class);
+                        criteria3.add(Restrictions.eq("user", user));
+                        criteria3.add(Restrictions.eq("gig_Status", gig_Status));
+
+                        if (!criteria3.list().isEmpty()) {
+                            List<Gig> userGigsList = criteria3.list();
+
+                            for (Gig gig : userGigsList) {
+                                allPendingGigsList.add(gig);
+                            }
+                        }
+                    }
+
+                    if (allPendingGigsList.size() >= 1) {
+                        responseObject.addProperty("status", true);
+                        responseObject.add("allPendingGigsList", gson.toJsonTree(allPendingGigsList));
+                    } else {
+                        responseObject.addProperty("message", "EMPTY");
+                    }
+                } else {
+                    responseObject.addProperty("message", "EMPTY");
+                }
+            } else {
+                responseObject.addProperty("message", "EMPTY");
+            }
+        }
+
+        session.close();
+
+        String responseText = gson.toJson(responseObject);
+        response.setContentType("application/json");
+        response.getWriter().write(responseText);
+    }
+}
---
+++ src/java/controller/VerifyGig.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import entity.Gig;
+import entity.Gig_Status;
+import hibernate.HibernateUtil;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Mail;
+import model.Util;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author User
+ */
+@WebServlet(name = "VerifyGig", urlPatterns = {"/VerifyGig"})
+public class VerifyGig extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(request.getReader(), JsonObject.class);
+
+        int gigId = jsonObject.get("id").getAsInt();
+
+        JsonObject responseObject = new JsonObject();
+        responseObject.addProperty("status", false);
+
+        if (gigId != 0) {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+
+            Criteria criteria = session.createCriteria(Gig.class);
+            criteria.add(Restrictions.eq("id", gigId));
+
+            if (!criteria.list().isEmpty()) {
+                Gig gig = (Gig) criteria.list().get(0);
+
+                if (gig.getStatus().getValue().equals("Pending")) {
+                    Criteria criteria1 = session.createCriteria(Gig_Status.class);
+                    criteria1.add(Restrictions.eq("value", "Verified"));
+
+                    if (!criteria1.list().isEmpty()) {
+                        Gig_Status gig_Status = (Gig_Status) criteria1.list().get(0);
+
+                        gig.setStatus(gig_Status);
+                        session.merge(gig);
+                        session.beginTransaction().commit();
+
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+                        String verificationEmailTemplatePath = getServletContext().getRealPath("/assets/templates/emails/VerifiedGig.html");
+                        String verificationEmailTemplate = Util.loadEmailTemplate(verificationEmailTemplatePath);
+
+                        String logoURL = "https://raw.githubusercontent.com/thisalsalpura/hireup_backend/master/web/assets/icons/logo.png";
+                        String facebookURL = "https://raw.githubusercontent.com/thisalsalpura/hireup_backend/master/web/assets/icons/facebook.png";
+                        String instagramURL = "https://raw.githubusercontent.com/thisalsalpura/hireup_backend/master/web/assets/icons/instagram.png";
+                        String linkedinURL = "https://raw.githubusercontent.com/thisalsalpura/hireup_backend/master/web/assets/icons/linkedin.png";
+                        String xtwitterURL = "https://raw.githubusercontent.com/thisalsalpura/hireup_backend/master/web/assets/icons/x-twitter.png";
+                        String youtubeURL = "https://raw.githubusercontent.com/thisalsalpura/hireup_backend/master/web/assets/icons/youtube.png";
+
+                        if (!verificationEmailTemplate.isEmpty()) {
+                            String filledVerificationEmailTemplate = verificationEmailTemplate
+                                    .replace("{{logo}}", logoURL)
+                                    .replace("{{date}}", sdf.format(new Date()))
+                                    .replace("{{name}}", gig.getUser().getFname() + " " + gig.getUser().getLname())
+                                    .replace("{{gigTitle}}", gig.getTitle())
+                                    .replace("{{facebookIcon}}", facebookURL)
+                                    .replace("{{instagramIcon}}", instagramURL)
+                                    .replace("{{linkedinIcon}}", linkedinURL)
+                                    .replace("{{x-twitterIcon}}", xtwitterURL)
+                                    .replace("{{youtubeIcon}}", youtubeURL);
+                            
+                            final String email = gig.getUser().getEmail();
+
+                            Runnable r = new Runnable() {
+                                @Override
+                                public void run() {
+                                    Mail.sendMail(email, "HireUp - Verified Gig", filledVerificationEmailTemplate);
+                                }
+                            };
+                            Thread t = new Thread(r);
+                            t.start();
+
+                            responseObject.addProperty("status", true);
+                            responseObject.addProperty("message", gig.getTitle() + " set as a Verified Gig.");
+                        }
+                    }
+                }
+            }
+
+            session.close();
+        }
+
+        String responseText = gson.toJson(responseObject);
+        response.setContentType("application/json");
+        response.getWriter().write(responseText);
+    }
+}
